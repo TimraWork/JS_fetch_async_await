bcd8f76288023c866e26f594938e21d24958f7e3
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-04-13 16:05:42 +0500

    ASYNC function + AWAIT es6 syntax to PROMISE

--- index.js
+++ index.js
@@ -2,36 +2,23 @@ const delay = (ms) => {
 	return new Promise((r) => setTimeout(() => r(), ms));
 };
 
-// delay(2000).then(() => console.log('2 sec'));
-
 const url = 'https://jsonplaceholder.typicode.com/todos';
-
-// function fetchTodos() {
-// 	console.log('Fetch todo started ...');
-// 	// создадим искусственную задержку
-// 	return delay(2000)
-// 		.then(() => {
-// 			// через 2s сделаем запрос на сервер и вернем результат в формате json
-// 			// fetch(url).then(response => response.json());
-
-// 			// возвращаем fetch
-// 			return fetch(url);
-// 		})
-// 		.then((response) => response.json());
-// }
-
-// fetchTodos()
-// 	.then((data) => {
-// 		console.log('Data: ', data);
-// 	})
-// 	.catch((e) => console.error(e));
 
 async function fetchAsyncTodos() {
 	// если внутри ф-и мы используем await, то сама ф-я должна быть ассинхронной - async
-	await delay(2000); // ждем 2s
-	const response = await fetch(url); // ждем запрос
-	const data = await response.json(); // ждем ответ
-	console.log('Data: ', data);
+	try {
+		// ЕСЛИ ВСЕ ХОРОШО, то срабатывает TRY
+		await delay(2000); // ждем 2s
+		const response = await fetch(url); // ждем запрос
+		const data = await response.json(); // ждем ответ
+		console.log('Data: ', data); // выводим ответ в консольь
+	} catch (e) {
+		// ЕСЛИ ЧТО-ТО пошло не так, то выводим ошибку в консоль
+		console.log(e);
+	} finally {
+		// ЭТОТ КОД ВЫПОЛНЯЕТСЯ ВНЕ ЗАВИМОСТИ ОТ ОШИБОК ВСЕГДА ВКОНЦЕ
+		console.log('finally');
+	}
 }
 
 fetchAsyncTodos(); // запускаем ф-ю

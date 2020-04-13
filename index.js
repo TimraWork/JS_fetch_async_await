22b4ef3c6a44b3ac1c79e7cf963fb73f918a44d2
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-04-13 15:42:49 +0500

    ASYNC function and AWAIT result

--- index.js
+++ index.js
@@ -2,26 +2,36 @@ const delay = (ms) => {
 	return new Promise((r) => setTimeout(() => r(), ms));
 };
 
-delay(2000).then(() => console.log('2 sec'));
+// delay(2000).then(() => console.log('2 sec'));
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
-function fetchTodos() {
-	console.log('Fetch todo started ...');
-	// создадим искусственную задержку
-	return delay(2000)
-		.then(() => {
-			// через 2s сделаем запрос на сервер и вернем результат в формате json
-			// fetch(url).then(response => response.json());
+// function fetchTodos() {
+// 	console.log('Fetch todo started ...');
+// 	// создадим искусственную задержку
+// 	return delay(2000)
+// 		.then(() => {
+// 			// через 2s сделаем запрос на сервер и вернем результат в формате json
+// 			// fetch(url).then(response => response.json());
 
-			// возвращаем fetch
-			return fetch(url);
-		})
-		.then((response) => response.json());
+// 			// возвращаем fetch
+// 			return fetch(url);
+// 		})
+// 		.then((response) => response.json());
+// }
+
+// fetchTodos()
+// 	.then((data) => {
+// 		console.log('Data: ', data);
+// 	})
+// 	.catch((e) => console.error(e));
+
+async function fetchAsyncTodos() {
+	// если внутри ф-и мы используем await, то сама ф-я должна быть ассинхронной - async
+	await delay(2000); // ждем 2s
+	const response = await fetch(url); // ждем запрос
+	const data = await response.json(); // ждем ответ
+	console.log('Data: ', data);
 }
 
-fetchTodos()
-	.then((data) => {
-		console.log('Data: ', data);
-	})
-	.catch((e) => console.error(e));
+fetchAsyncTodos(); // запускаем ф-ю
